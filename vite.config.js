4d26f99cc282e65ba0cf3733d2a207c71c564c13
ammarsetiawan <ammarsetiawan970@gmail.com>
2025-11-11 17:11:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pokemon-project/', // ⚠️ ganti dengan nama repo GitHub kamu
+  base: '/pokemon-project/', // ← pakai nama repo kamu persis di sini
 })
